a2418e124cfafc2587e0540b877647a04b466416
krishan32 <krm219@lehigh.edu>
2015-08-28 19:20:46 +0000

    updated lab1

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,18 @@
+//author: Krishan Madan
+//class: CSE002
+//professor: Brian Chen
+public class HelloWorld {
+
+public static void main (String [] args) {
+    
+System.out.println ("##################");
+System.out.println ("##################");
+System.out.println ("#");
+System.out.println("Hello World"); //prints Hello World
+System.out.println ("#");
+System.out.println ("##################");
+System.out.println ("##################");
+
+}
+//end of test
+}
